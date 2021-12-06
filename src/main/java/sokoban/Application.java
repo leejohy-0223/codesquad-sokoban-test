7e131b71f4a9dcaecb3396824e5a6830bb0f3604
leejohy-0223 <leejohy@naver.com>
2021-12-06 19:18:58 +0900

    Add main method in Application.java
    * 전체적인 실행 흐름 구성

--- src/main/java/sokoban/Application.java
+++ src/main/java/sokoban/Application.java
@@ -1,4 +1,27 @@
 package sokoban;
 
 public class Application {
+
+    public static void main(String[] args) {
+        String input = "Stage 1\n"
+            + "#####\n"
+            + "#OoP#\n"
+            + "#####\n"
+            + "=====\n"
+            + "Stage 2\n"
+            + "  #######\n"
+            + "###  O  ###\n"
+            + "#    o    #\n"
+            + "# Oo P oO #\n"
+            + "###  o  ###\n"
+            + " #   O  # \n"
+            + " ########";
+
+        StageMapReader mapReader = StageMapReader.initialMapReader();
+        mapReader.mappingTwoDimensionalArray(input);
+        mapReader.printStageInfo();
+    }
+
+
+
 }
--- src/main/java/sokoban/MapReader.java
+++
@@ -1,4 +0,0 @@
-package sokoban;
-
-public class MapReader {
-}
