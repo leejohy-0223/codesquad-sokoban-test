2efe8b9cd05baa4d70d70c30988d5e51ae32a5f2
leejohy-0223 <leejohy@naver.com>
2021-12-06 20:35:13 +0900

    Add PlayerPosition class
    * Player의 위치 정보를 가지는 class 생성

---
+++ src/main/java/sokoban/PlayerPosition.java
@@ -0,0 +1,11 @@
+package sokoban;
+
+public class PlayerPosition {
+    int posX;
+    int posY;
+
+    public PlayerPosition(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+}
