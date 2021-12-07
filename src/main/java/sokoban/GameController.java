f828c67f7aa040b8046d163343b267d03f17c72d
leejohy-0223 <leejohy@naver.com>
2021-12-07 14:32:33 +0900

    Add GameController gameStart method
    * 게임을 시작하고 사용자의 입력을 받아 StageMap에 명령을 내리는 메서드를 구현한다.

--- src/main/java/sokoban/GameController.java
+++ src/main/java/sokoban/GameController.java
@@ -1,7 +1,47 @@
 package sokoban;
 
+import java.util.List;
+
 public class GameController {
     public static void gameStart(StageMap stageMap) {
+        stageMap.printStatus();
+        List<Character> inputs = InputView.requestInputFromUser();
 
+        while (true) {
+            for (Character input : inputs) {
+                moveByInput(stageMap, input);
+            }
+            if (inputs.contains('q')) {
+                break;
+            }
+            inputs = InputView.requestInputFromUser();
+        }
+    }
+
+    private static void moveByInput(StageMap stageMap, Character input) {
+        DirectionValue dValue = mappingToDirectionValue(input);
+        if (dValue == DirectionValue.INVALID) {
+            printInvalidCommand(stageMap, input);
+            return;
+        }
+        if (dValue == DirectionValue.QUIT) {
+            System.out.println(dValue.getDirectionName());
+            return;
+        }
+        stageMap.movePlayer(dValue);
+    }
+
+    private static DirectionValue mappingToDirectionValue(Character input) {
+        for (DirectionValue value : DirectionValue.values()) {
+            if (value.getSign() == input) {
+                return value;
+            }
+        }
+        return DirectionValue.INVALID;
+    }
+
+    private static void printInvalidCommand(StageMap stageMap, Character input) {
+        stageMap.printOnlyStageMap();
+        System.out.println(Character.toUpperCase(input) + ": (경고!) 해당 명령을 수행할 수 없습니다!");
     }
 }
