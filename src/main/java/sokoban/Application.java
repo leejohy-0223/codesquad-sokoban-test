b5fac9b9c20683053b47a8f478d6918cf758d0ab
leejohy-0223 <leejohy@naver.com>
2021-12-07 18:48:40 +0900

    Move File input role to InputView
    * InputView : File의 입력을 받는 로직을 InputView로 이동

--- src/main/java/sokoban/Application.java
+++ src/main/java/sokoban/Application.java
@@ -1,29 +1,13 @@
 package sokoban;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Application {
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        try {
-            FileReader fr = new FileReader("src/main/resources/stageMap.txt");
-            BufferedReader br = new BufferedReader(fr);
-            sb = new StringBuilder();
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        String mapInput = InputView.makeMapInformation();
         StageMapReader mapReader = StageMapReader.initialMapReader(new ArrayList<>());
-        mapReader.mappingTwoDimensionalArray(sb.toString());
-        mapReader.printStageInfo();
+        mapReader.mappingTwoDimensionalArray(mapInput);
         mapReader.startThisStage("Stage 2");
     }
 }
--- src/main/java/sokoban/InputView.java
+++ src/main/java/sokoban/InputView.java
@@ -1,5 +1,8 @@
 package sokoban;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -15,4 +18,19 @@ public class InputView {
         String input = sc.nextLine();
         return input.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
     }
+
+    public static String makeMapInformation() {
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/stageMap.txt"));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
 }
