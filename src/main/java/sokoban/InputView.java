1bc9b37a9350de81cf6ee6cff65e77daae2e68e1
leejohy-0223 <leejohy@naver.com>
2021-12-08 01:15:01 +0900

    Change InputView for receive input from runtime
    * main 함수의 인자로 지도 파일을 받아서 처리할 수 있도록 수정

--- src/main/java/sokoban/InputView.java
+++ src/main/java/sokoban/InputView.java
@@ -1,5 +1,7 @@
 package sokoban;
 
+import static sokoban.Constant.*;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,22 +10,11 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
-
-    public static final String PROMPT = "SOKOBAN> ";
-
-    public static List<Character> requestInputFromUser() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print(PROMPT);
-
-        String input = sc.nextLine();
-        return input.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
-    }
-
-    public static String makeMapInformation() {
+    public static String makeMapInformation(String fileName) {
         StringBuilder sb = new StringBuilder();
         try {
-            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/stageMap.txt"));
-            String line = "";
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
             while ((line = br.readLine()) != null) {
                 sb.append(line).append("\n");
             }
@@ -34,4 +25,10 @@ public class InputView {
         return sb.toString();
     }
 
+    public static List<Character> requestInputFromUser() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(PROMPT);
+        String input = sc.nextLine();
+        return input.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
+    }
 }
